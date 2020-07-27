1655736444c7b6782c6abdae4e29368ef5ab8738
Sameer Argade <sameer.argade@dws.com.au>
2020-07-27 10:24:25 +1000

    Initial Commit

---
+++ src/main/java/com/argade/mspracticegrocerylistservice/MsPracticeGroceryListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.argade.mspracticegrocerylistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPracticeGroceryListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPracticeGroceryListServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argade/mspracticegrocerylistservice/MsPracticeGroceryListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argade.mspracticegrocerylistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPracticeGroceryListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
